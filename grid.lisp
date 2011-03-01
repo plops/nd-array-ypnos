8e8adbcd2c74dd07b963f3d1c143c4c79b4842d5
martin <martin@hop.kcl.ac.uk>
2011-03-01 06:00:02 +0000

    star generator

--- grid.lisp
+++ grid.lisp
@@ -21,6 +21,19 @@
 			   `(,e (+ i ,j))))
 	      ,@body)))))
 
+#+nil
 (let ((a (make-array 5 :element-type 'single-float)))
-  (do-grid (a (l c r))
+  (do-grid (a #.(make-star 3))
     (setf (aref a c) (+ (aref a l) (aref a r)))))
+
+(defun make-star (n)
+  (cond
+    ((and (numberp n) (= 3 n)) '(l c r))
+    ((and (numberp n) (= 5 n)) '(ll l c r rr))
+    ((and (consp n) (equal n '(3 3))) 
+     '((ul uc ur)
+       (cl cc cr)
+       (ll lc lr)))
+    (t (error "don't know how to make this star"))))
+#+nil
+(make-star '(3 3))
